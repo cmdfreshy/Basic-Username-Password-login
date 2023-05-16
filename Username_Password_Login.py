f98c5cb7f5c3bef2c5650dc5e85c7c452cc43f70
cmdfreshy <nullfreshy@gmail.com>
2023-05-16 20:21:52 +1200

    Add project files.

---
+++ Username_Password_Login.py
@@ -0,0 +1,19 @@
+def authenticate():
+    # Predefined username and password
+    correct_username = "admin"
+    correct_password = "password123"
+
+    for i in range(5):
+        username = input("Enter your username: ")
+        password = input("Enter your password: ")
+
+        if username == correct_username and password == correct_password:
+            print("Authentication successful!")
+            return
+
+        print("Authentication failed. Please try again.")
+
+    print("Maximum number of attempts reached.")
+
+# Call the authenticate function to start the authentication process
+authenticate()
